5a428eab555389df58a85ae3496935045d2a794a
hassanjaved00122 <hassanjaved00122@gmail.com>
2026-07-11 23:29:14 -0700

    test

--- app.py
+++ app.py
@@ -116,6 +116,5 @@ if st.button("Predict Price"):
     input_scaled = scaler.transform(input_df)
 
     # Prediction
-    prediction = model.predict(input_scaled)[0]khewiugfcewhjke;odjewkckewjlwjfirwclkw
-
+    prediction = model.predict(input_scaled)[0]
     st.success(f"🏠 Estimated House Price: ${prediction:,.2f}")
